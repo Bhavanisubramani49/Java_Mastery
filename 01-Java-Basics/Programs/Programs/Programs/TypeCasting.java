2e6ca802df5a79a2e49da499399bcb4952739f5f
Bhavani S <bhavanisubramani49@gmail.com>
2026-08-04 23:20:45 +0530

    Add TypeCasting.java for demonstrating type casting

---
+++ 01-Java-Basics/Programs/Programs/Programs/TypeCasting.java
@@ -0,0 +1,19 @@
+public class TypeCasting {
+
+    public static void main(String[] args) {
+
+        int num = 25;
+
+        double value = num;
+
+        System.out.println("Implicit : " + value);
+
+        double pi = 3.14;
+
+        int integerValue = (int) pi;
+
+        System.out.println("Explicit : " + integerValue);
+
+    }
+
+}
