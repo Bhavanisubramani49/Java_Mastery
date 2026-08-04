48d8724254288e148a354782d22dc6363015a82f
Bhavani S <bhavanisubramani49@gmail.com>
2026-08-04 23:24:58 +0530

    Add EscapeSequence.java demonstrating escape sequences

---
+++ 01-Java-Basics/Programs/01-Java-Basics/Programs/Programs/Programs/EscapeSequence.java
@@ -0,0 +1,15 @@
+public class EscapeSequence {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello\nJava");
+
+        System.out.println("Welcome\tBhavani");
+
+        System.out.println("\"Java\"");
+
+        System.out.println("C:\\Users\\Bhavani");
+
+    }
+
+}
