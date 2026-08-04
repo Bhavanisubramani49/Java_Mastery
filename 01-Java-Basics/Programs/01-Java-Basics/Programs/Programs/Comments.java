148fc172e50a80480e4f8bce40c2e71266c7f77c
Bhavani S <bhavanisubramani49@gmail.com>
2026-08-04 23:24:04 +0530

    Create Comments.java to illustrate Java comments
    Added a Java class demonstrating single-line, multi-line, and documentation comments.

---
+++ 01-Java-Basics/Programs/01-Java-Basics/Programs/Programs/Comments.java
@@ -0,0 +1,19 @@
+public class Comments {
+
+    public static void main(String[] args) {
+
+        // Single-line comment
+
+        /*
+           Multi-line comment
+        */
+
+        /**
+         * Documentation Comment
+         */
+
+        System.out.println("Java Comments Example");
+
+    }
+
+}
