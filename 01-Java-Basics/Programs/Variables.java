f923aa1907a96f7b57ed3683e0e41813dd48a4fa
Bhavani S <bhavanisubramani49@gmail.com>
2026-08-04 23:19:09 +0530

    Add Variables.java to demonstrate variable types
    This program demonstrates the use of various variable types in Java, including int, double, char, boolean, and String.

---
+++ 01-Java-Basics/Programs/Variables.java
@@ -0,0 +1,19 @@
+public class Variables {
+
+    public static void main(String[] args) {
+
+        int age = 20;
+        double salary = 35000.75;
+        char grade = 'A';
+        boolean placed = true;
+        String name = "Bhavani";
+
+        System.out.println("Name    : " + name);
+        System.out.println("Age     : " + age);
+        System.out.println("Salary  : " + salary);
+        System.out.println("Grade   : " + grade);
+        System.out.println("Placed  : " + placed);
+
+    }
+
+}
