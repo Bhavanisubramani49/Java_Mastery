2ee3414a6ebf653c87a841d342aec26f113bc8d6
Bhavani S <bhavanisubramani49@gmail.com>
2026-08-04 23:19:51 +0530

    Add DataTypes class to demonstrate Java data types

---
+++ 01-Java-Basics/Programs/Programs/DataTypes.java
@@ -0,0 +1,27 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        byte a = 100;
+        short b = 20000;
+        int c = 100000;
+        long d = 9876543210L;
+
+        float e = 12.5f;
+        double f = 45.789;
+
+        char g = 'A';
+        boolean h = true;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+
+    }
+
+}
