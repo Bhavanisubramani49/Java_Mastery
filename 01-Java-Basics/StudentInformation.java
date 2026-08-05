029121bb7e9adb6a3fbfc0e6c5f01b8e54abdbce
Bhavani S <bhavanisubramani49@gmail.com>
2026-08-05 23:22:36 +0530

    Add StudentInformation class to gather student data
    This program collects and displays student information including name, age, and CGPA.

---
+++ 01-Java-Basics/StudentInformation.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class StudentInformation {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Name : ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Age : ");
+        int age = sc.nextInt();
+
+        System.out.print("Enter CGPA : ");
+        double cgpa = sc.nextDouble();
+
+        System.out.println("\ ==STUDENT REPORT==);
+        System.out.println("Name : " + name);
+        System.out.println("Age  : " + age);
+        System.out.println("CGPA : " + cgpa);
+
+        sc.close();
+    }
+}
