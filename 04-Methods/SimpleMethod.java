f81c1c3299dcbe59d0a4f98f125ec5c3957a2552
Bhavani S <bhavanisubramani49@gmail.com>
2026-08-11 20:26:21 +0530

    Add SimpleMethod class with a greeting method

---
+++ 04-Methods/SimpleMethod.java
@@ -0,0 +1,10 @@
+public class SimpleMethod {
+
+    static void greet() {
+        System.out.println("Welcome to Java Methods");
+    }
+
+    public static void main(String[] args) {
+        greet();
+    }
+}
