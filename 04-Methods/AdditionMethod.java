859e9c801c95b50e4f60795a3a11f62325d42d55
Bhavani S <bhavanisubramani49@gmail.com>
2026-08-11 20:27:40 +0530

    Implement addition method in AdditionMethod class

---
+++ 04-Methods/AdditionMethod.java
@@ -0,0 +1,13 @@
+public class AdditionMethod {
+
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+
+        int result = add(25, 35);
+
+        System.out.println("Sum = " + result);
+    }
+}
