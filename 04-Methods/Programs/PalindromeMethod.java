bfaac479f86e75d7daed359076907ae4806cb773
Bhavani S <bhavanisubramani49@gmail.com>
2026-08-11 20:43:07 +0530

    Add PalindromeMethod to check for palindromes

---
+++ 04-Methods/Programs/PalindromeMethod.java
@@ -0,0 +1,28 @@
+public class PalindromeMethod {
+
+    static boolean isPalindrome(int number) {
+
+        int original = number;
+        int reverse = 0;
+
+        while (number > 0) {
+
+            int digit = number % 10;
+            reverse = reverse * 10 + digit;
+            number /= 10;
+        }
+
+        return original == reverse;
+    }
+
+    public static void main(String[] args) {
+
+        int number = 121;
+
+        if (isPalindrome(number)) {
+            System.out.println(number + " is a Palindrome");
+        } else {
+            System.out.println(number + " is Not a Palindrome");
+        }
+    }
+}
