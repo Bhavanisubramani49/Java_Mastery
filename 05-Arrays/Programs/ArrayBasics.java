660730c642db7c939a15488003eadd0acddf0f58
Bhavani S <bhavanisubramani49@gmail.com>
2026-08-12 22:52:13 +0530

    Add ArrayBasics.java to demonstrate array usage

---
+++ 05-Arrays/Programs/ArrayBasics.java
@@ -0,0 +1,15 @@
+public class ArrayBasics {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        System.out.println("Array Elements:");
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Index " + i + " : " + numbers[i]);
+        }
+
+        System.out.println("Array Length: " + numbers.length);
+    }
+}
